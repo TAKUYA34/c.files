2bf36bd99b9128d48b7082a68bae494a5f7be47a
takuya-Y <hakenad907@gmail.com>
2024-08-18 13:17:01 +0900

    ファイルデータの集計

--- fileStream.c
+++ fileStream.c
@@ -1,7 +1,7 @@
 // ファイルのオープンとクローズ
 #include <stdio.h>
 
-#if (1)
+#if (0)
 
 int main(void)
 {
@@ -17,6 +17,41 @@ int main(void)
   {
     printf("\aファイルをオープンしました。\n");
     fclose(fp);
+  }
+  return 0;
+}
+#endif
+
+#if (1)
+
+#define MAX 100
+
+int main(void)
+{
+  FILE* fp;
+
+  if ((fp = fopen("abc.txt", "r")) == NULL)
+  {
+    printf("\aファイルのオープンに失敗しました。\n");
+  }
+  else
+  {
+    int numOP = 0;
+    char name[MAX];
+    double height, wight;
+    double hsum = 0.0;
+    double wsum = 0.0;
+
+    while (fscanf(fp, "%s%lf%lf", name, &height, &wight) == 3)
+    {
+      printf("%-10s %5.1f %5.1f\n", name, height, wight);
+      numOP++;
+      hsum += height;
+      wsum += wight;
+    }
+  printf("----------------------\n");
+  printf("平均       %5.1f %5.1f\n", hsum / numOP, wsum / numOP);
+  fclose(fp);
   }
   return 0;
 }
